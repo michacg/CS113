b49c803a5aa237489b4ecdd8afd3d94d7730bf3f
Michaela Gonzales <michacg2@uci.edu>
2019-10-27 20:45:33 -0700

    NPC FSM works for moving place to place

--- CS113/Assets/Scripts/NpcController.cs
+++ CS113/Assets/Scripts/NpcController.cs
@@ -4,24 +4,96 @@ using UnityEngine;
 
 public class NpcController : MonoBehaviour
 {
-    List<Transform> locations = new List<Transform>();
+ 
     //State for our Finite State Machine
     public enum State
     {
-        normal,
+        walking,
+        Action,
         suspicious,
         scared,
     };
 
+    //Add Locations you want NPC to travel to.
+    [SerializeField] List<GameObject> locations = new List<GameObject>();
     [SerializeField] float lookRadius = 5f;
+    [SerializeField] float speed;
+
+    //The min and max of these fields will be chosen by random during Action State.
+    [SerializeField] float actionTimeSecMinimum = 3.0f;
+    [SerializeField] float actionTimeSecMaximum = 8.0f;
+
+    private State currentState;
+    private int i = 0;
+
     void Start()
     {
-        
+        currentState = State.walking;
     }
 
     
     void Update()
     {
-        
+        print(currentState);
+        switch(currentState)
+        {
+            case State.walking:
+                WalkingUpdate();
+                break;
+
+            case State.Action:
+                ActionUpdate();
+                break;
+
+            case State.suspicious:
+                SuspiciousUpdate();
+                break;
+
+            case State.scared:
+                ScaredUpdate();
+                break;
+        }
+    }
+
+    void WalkingUpdate()
+    {
+        transform.LookAt(locations[i].transform);
+        float distance = Vector3.Distance(locations[i].transform.position, transform.position);
+        print("DISTANCE: "+ distance);
+
+        if (distance > 1) //Not at Location
+        {
+            print("walkin");
+            transform.position = Vector3.MoveTowards(transform.position, locations[i].transform.position, speed);
+        }
+        else //At Loc. NPC will perform action
+        {
+            StartCoroutine("DoAction");
+        }
+
+    }
+
+    IEnumerator DoAction()
+    {
+        currentState = State.Action;
+        float secondsDoingAction = Random.Range(actionTimeSecMinimum, actionTimeSecMaximum);
+        yield return new WaitForSeconds(secondsDoingAction);
+        i = (i + 1) % locations.Count;
+        currentState = State.walking;
+    }
+
+    void ActionUpdate()
+    {
+        //Animations / indicating the User they're distracted
+    }
+
+    void SuspiciousUpdate()
+    {
+
+    }
+
+    void ScaredUpdate()
+    {
+
     }
 }
