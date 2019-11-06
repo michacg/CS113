091810a62dae70931f3b4598839aaf6d32ba992c
cwang1237 <47485660+cwang1237@users.noreply.github.com>
2019-11-06 08:33:59 -0800

    ScareMeter

---
+++ CS113/Assets/Scripts/ScareMeter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScareMeter : MonoBehaviour
+{
+    [SerializeField]
+    private float fillAmount;
+
+    [SerializeField]
+    private Image meter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Handle();
+    }
+
+    private void Handle()
+    {
+        meter.fillAmount = fillAmount;
+    }
+
+    private float ChangeVal(float value, float min, float max, float mmin, float mmax)
+    {
+        return (value - min) * (mmax - mmin) / (max - min) + mmin;
+    }
+}
