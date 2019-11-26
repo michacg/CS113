8912ba6b4ac6fb9229dc817b82a3037e69284682
michacg <michacg2@uci.edu>
2019-11-25 20:11:19 -0800

    Start menus and such (bare bones) all done. Its ugly right now though sorry

---
+++ CS113/Assets/Scripts/Menu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Cynthia");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ CS113/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pausemenu;
+
+    bool paused = false;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            //Debug.Log("HI");
+            if (!paused)
+            {
+                pausemenu.SetActive(true);
+                Time.timeScale = 0;
+            }
+            else
+            {
+                pausemenu.SetActive(false);
+                Time.timeScale = 1;
+            }
+
+            paused = !paused;
+        }
+    }
+
+    //for the continue button
+    public void onUnpause()
+    {
+        Unpause();
+    }
+
+    void Unpause()
+    {
+        Time.timeScale = 1;
+        
+        pausemenu.SetActive(false);
+        paused = !paused;
+    }
+
+}
