0c8d3771863c20d0e0c77935cee433e03ed9bb2c
cyntsao <cyntsao@users.noreply.github.com>
2019-11-26 19:56:34 -0800

    tutorial/controls

---
+++ CS113/Assets/Scripts/Tutorial.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Tutorial : MonoBehaviour
+{
+    [SerializeField] GameObject move_tut = null;
+    [SerializeField] GameObject float_tut = null;
+    [SerializeField] GameObject possess_tut = null;
+    [SerializeField] GameObject unpossess_tut = null;
+    
+    [SerializeField] GameObject movement_control = null;
+    
+    void Start()
+    {
+        StartCoroutine("tutorial");
+    }
+
+    void Update()
+    {
+        
+    }
+    
+    IEnumerator tutorial()
+    {
+        // MOVE
+        move_tut.SetActive(true);
+        StartCoroutine(textTransition(move_tut));
+        while (true)
+        {
+            float v = Input.GetAxis("Vertical");
+            float h = Input.GetAxis("Horizontal");
+            yield return new WaitForSeconds(0.1f);
+            if (v != 0 || h != 0)
+            {
+                yield return new WaitForSeconds(0.8f);
+                break;
+            }
+        }
+        move_tut.SetActive(false);
+        yield return new WaitForSeconds(1f);
+        
+        // FLOAT
+        float_tut.SetActive(true);
+        StartCoroutine(textTransition(float_tut));
+        while (true)
+        {
+            yield return new WaitForSeconds(0.1f);
+            if (Input.GetKey(KeyCode.Space))
+            {
+                yield return new WaitForSeconds(0.8f);
+                break;
+            }
+        }
+        float_tut.SetActive(false);
+        yield return new WaitForSeconds(1f);
+        
+        // POSSESS
+        possess_tut.SetActive(true);
+        StartCoroutine(textTransition(possess_tut));
+        while (true)
+        {
+            yield return new WaitForSeconds(0.1f);
+            if (movement_control.transform.childCount > 1)
+            {
+                yield return new WaitForSeconds(0.8f);
+                break;
+            }
+        }
+        possess_tut.SetActive(false);
+        yield return new WaitForSeconds(1f);
+        
+        // UNPOSSESS
+        unpossess_tut.SetActive(true);
+        StartCoroutine(textTransition(unpossess_tut));
+        while (true)
+        {
+            yield return new WaitForSeconds(0.1f);
+            if (movement_control.transform.childCount == 1)
+            {
+                yield return new WaitForSeconds(0.8f);
+                break;
+            }
+        }
+        unpossess_tut.SetActive(false);
+        
+        yield return null;
+    }
+    
+    IEnumerator textTransition(GameObject go)
+    {
+        TextMeshProUGUI TEXT = go.GetComponent<TextMeshProUGUI>();
+        
+        string t = TEXT.text;
+        TEXT.text = "";
+        yield return new WaitForSeconds(0.05f);
+        for (int i = 0; i < t.Length; i++)
+        {
+            TEXT.text += t[i];
+            yield return new WaitForSeconds(0.05f);
+        }
+        
+        yield return null;
+    }
+}
