63962309661ef6e0359df34d9a6a35ba005980fb
cwang1237 <47485660+cwang1237@users.noreply.github.com>
2019-11-30 18:00:18 -0800

    StartMenu Changes

---
+++ CS113/Assets/Scripts/Hover.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float horizontal;
+    public float vertical;
+    public float apt;
+    
+    public Vector3 pos;
+
+    void Start()
+    {
+        pos = transform.position;
+        StartCoroutine(startWait());
+    }
+
+    IEnumerator startWait()
+    {
+        yield return new WaitForSeconds(1000);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float curr = Time.realtimeSinceStartup;
+        if(curr < 1 || vertical == 0 || apt == 0)
+        {
+            pos = transform.position;
+        }
+        else
+        {
+            pos.x += horizontal;
+            pos.y += Mathf.Sin(Time.realtimeSinceStartup * vertical) * apt;
+        }
+        transform.position = pos;
+    }
+}
